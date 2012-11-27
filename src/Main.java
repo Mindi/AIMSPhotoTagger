8435841fe2b6b46e7a9ba6c735eeb78403bd0706
timothyhart <timothy.hart@my.jcu.edu.au>
2012-11-27 12:28:21 +1000

    nearly working code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+
+
+ import javax.persistence.*;
+ import java.util.List;
+
 public class Main {
+    private static final String PERSISTENCE_UNIT_NAME = "REEF-DERBY";
+    private static EntityManagerFactory factory;
 
     public static void main(String[] args) {
-	// write your code here
-    }
+         factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+
+        Query q = em.createQuery("SELECT Keyword FROM REEFMON.Keywords;");
+        List<String> keywords = q.getResultList();
+
+        for (String keyword : keywords){
+            System.out.println(keyword);
+        }
+         }
 }
