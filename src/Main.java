c64e1396a2cdab8956d2302b4db8a24659f61055
timothyhart <timothy.hart@my.jcu.edu.au>
2012-11-27 15:35:28 +1000

    yay it works some more

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 
  import javax.persistence.*;
+ import java.sql.Statement;
  import java.util.List;
+ import java.sql.ResultSet;
 
 public class Main {
     private static final String PERSISTENCE_UNIT_NAME = "REEF-DERBY";
@@ -12,10 +14,19 @@ public class Main {
         EntityManager em = factory.createEntityManager();
 
         Query q = em.createNativeQuery("SELECT Keyword FROM Keywords;");
-        List<String> keywords = q.getResultList();
+        List<String> keywords =  q.getResultList();
 
         for (String keyword : keywords){
             System.out.println(keyword);
+        }
+        q = em.createQuery("SELECT g FROM ReefGeolocale g ");
+
+        List<ReefGeolocale> reefs =q.getResultList();
+
+        for (ReefGeolocale reef : reefs){
+            System.out.println(reef.getReefName());
+            System.out.println(reef.getRmSectorByASector().getDesc1());
+
         }
          }
 }
